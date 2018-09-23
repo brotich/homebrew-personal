79dd2eed96601b5c6ea6d8a062b80445e6299248
Brian Rotich <23168146+brotich@users.noreply.github.com>
2018-09-23 04:19:11 +0300

    rename file to expected repo name

--- brew-personal.rb
+++ brew-personal.rb
@@ -1,7 +1,7 @@
 require 'formula'
 
-class BrewMycask < Formula
-  homepage 'https://github.com/brotich/homebrew-personal /'
+class BrewPersonal < Formula
+  homepage 'https://github.com/brotich/homebrew-personal/'
   url 'https://github.com/brotich/homebrew-personal.git'
   def install
   end

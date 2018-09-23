2871e116d7db922772b5264df0a447e5021f5e8d
Brian Rotich <23168146+brotich@users.noreply.github.com>
2018-09-23 04:13:52 +0300

    adding gpg-suite 2018.3

---
+++ brew-mycasks.rb
@@ -0,0 +1,8 @@
+require 'formula'
+
+class BrewMycask < Formula
+  homepage 'https://github.com/brotich/homebrew-mycask/'
+  url 'https://github.com/brotich/homebrew-mycask.git'
+  def install
+  end
+end
